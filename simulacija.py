955985e0e880888e0a995045705240aae873ec9e
Gaja Jamnik <gaja.jamnik@gmail.com>
2023-03-16 12:30:33 +0100

    izbris datoteke

--- simulacija.py
+++
@@ -1,7 +0,0 @@
-#simulacija strojev glede na vhoden podatke
-#VHODNI PODATKI: trenuten čas, datoteke s konfiguracijami strojev
-
-from stroj import Stroj, Meritev
-
-def simulacija_strojev(trenuten_čas, seznam_strojev):
-    pass
