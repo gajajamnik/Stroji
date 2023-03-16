03119bebb2144bd66306808bff23ca16e29c5f88
Gaja Jamnik <gaja.jamnik@gmail.com>
2023-03-16 12:30:24 +0100

    simulacija kot fun z vhodnimi podatki

--- main.py
+++ main.py
@@ -1,31 +1,51 @@
+#simulacija temperatur strojev glede na vhodne podatke: cas_interval, min_parameter
+
 import os
 from stroj import Stroj, Meritev
 from datetime import datetime
 import time
 
-#seznam vseh strojev
-stroji = []
 
-for ime_datoteke in os.listdir('stroji'):
-    stroj = Stroj.nalozi_stanje(os.path.join('stroji', ime_datoteke))
-    stroji.append(stroj)
+def simulacija(cas_interval, min_parameter):
+    #cas_interval = cas, ki ga opazujemo za simulacijo v desetinkah
+    #min_parameter = idealna temperatura stroja
+
+    #IZPIS IZ KONFIGURACIJSKIH DATOTEK
+    stroji = []
+    for ime_datoteke in os.listdir('stroji'):
+        stroj = Stroj.nalozi_stanje(os.path.join('stroji', ime_datoteke))
+        stroji.append(stroj)
 
 
-#časovni interval, ki nas zanima v desetinkah
-for i in range(20):
-    now = datetime.now()
-    trenuten_cas = now.time()
+    #SIMULACIJA
+    for i in range(cas_interval):
+        now = datetime.now()
+        trenuten_cas = now.time()
+        print(f'Trenuten cas je {trenuten_cas}')
+        print('----------------------------------')
+ 
+        for stroj in stroji:
+            print(f'ime stroja: {stroj.ime_stroja}')
+            print(f'stanje stroja: {stroj.stanje}')
+            print(f'temperatura: {stroj.trenutna_temperatura}')
 
-    #nastavimo ali je stroj prižgan ali ugasnjen
-    stroj.nastavi_trenutno_stanje(trenuten_cas)
+            #nastavimo ali je stroj prižgan ali ugasnjen
+            stroj.nastavi_trenutno_stanje(trenuten_cas)
+            print(f'novo stanje stroja: {stroj.stanje}')
 
-    #za vse stroje sporocimo temperaturo in poracunamo naslednjo
-    for stroj in stroji:
-        min_parameter = 10
-        stroj.sporoci_temperaturo(trenuten_cas, min_parameter)
+            #za vse stroje sporocimo temperaturo in poracunamo naslednjo
+            stroj.sporoci_temperaturo(trenuten_cas, min_parameter)
+            print(f'nova temperatura: {stroj.trenutna_temperatura}')
 
-    stroj.shrani_stanje(os.path.join('stroji', f'{stroj.ime_stroja}.json'))
+            stroj.shrani_stanje(os.path.join('stroji', f'{stroj.ime_stroja}.json'))
+            print(f'shranjujem stanje stroja {stroj.ime_stroja}')
+            print('----------------------------------------')
 
-    #spremembe se dogajajo na desetinko
-    time.sleep(0.100)
+        #notranja zanka potrbuje cca 0.003000 s
+        d = 0.003
 
+        #spremembe se dogajajo na desetinko
+        time.sleep(0.100 - d)
+
+
+simulacija(20, 10)
